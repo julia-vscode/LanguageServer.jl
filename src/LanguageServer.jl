aa4f5cab40f038e521ecc85384603f75ba5bc5cb
David Anthoff <anthoff@berkeley.edu>
2016-11-18 12:49:32 -0800

    Move LanguageServerInstance into separate file

--- src/LanguageServer.jl
+++ src/LanguageServer.jl
@@ -5,6 +5,7 @@ using JSON
 using Lint
 using URIParser
 
+include("languageserverinstance.jl")
 include("jsonrpc.jl")
 include("protocol.jl")
 include("parse.jl")
@@ -21,36 +22,6 @@ include("utilities.jl")
 type Document
     data::Vector{UInt8}
     blocks::Vector{Any}
-end
-
-type LanguageServerInstance
-    pipe_in
-    pipe_out
-
-    rootPath::String 
-    documents::Dict{String,Document}
-    DocStore::Dict{String,Any}
-
-    debug_mode::Bool
-
-    function LanguageServerInstance(pipe_in,pipe_out, debug_mode::Bool)
-        new(pipe_in,pipe_out,"",Dict{String,Document}(),Dict{String,Any}(), debug_mode)
-    end
-end
-
-function send(message, server)
-    message_json = JSON.json(message)
-
-    write_transport_layer(server.pipe_out,message_json, server)
-end
-
-function Base.run(server::LanguageServerInstance)
-    while true
-        message = read_transport_layer(server.pipe_in, server)
-        request = parse(Request, message)
-
-        process(request, server)
-    end
 end
 
 end
---
+++ src/languageserverinstance.jl
@@ -0,0 +1,29 @@
+type LanguageServerInstance
+    pipe_in
+    pipe_out
+
+    rootPath::String 
+    documents::Dict{String,Document}
+    DocStore::Dict{String,Any}
+
+    debug_mode::Bool
+
+    function LanguageServerInstance(pipe_in,pipe_out, debug_mode::Bool)
+        new(pipe_in,pipe_out,"",Dict{String,Document}(),Dict{String,Any}(), debug_mode)
+    end
+end
+
+function send(message, server)
+    message_json = JSON.json(message)
+
+    write_transport_layer(server.pipe_out,message_json, server)
+end
+
+function Base.run(server::LanguageServerInstance)
+    while true
+        message = read_transport_layer(server.pipe_in, server)
+        request = parse(Request, message)
+
+        process(request, server)
+    end
+end

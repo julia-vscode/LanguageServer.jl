639ae475bf7d79017ec8e137880553c00f99a969
Zac Nugent <zac.nugent@gmail.com>
2016-11-19 10:52:00 +0000

    LS instance reorder

--- src/languageserverinstance.jl
+++ src/languageserverinstance.jl
@@ -1,3 +1,35 @@
+type LanguageServerInstance
+    pipe_in
+    pipe_out
+
+    rootPath::String 
+    documents::Dict{String,Document}
+    DocStore::Dict{String,Any}
+
+    debug_mode::Bool
+
+    function LanguageServerInstance(pipe_in,pipe_out, debug_mode::Bool)
+        new(pipe_in,pipe_out,"",Dict{String,Document}(),Dict{String,Any}(), debug_mode)
+    end
+end
+
+function send(message, server)
+    message_json = JSON.json(message)
+
+    write_transport_layer(server.pipe_out,message_json, server.debug_mode)
+end
+
+function Base.run(server::LanguageServerInstance)
+    while true
+        message = read_transport_layer(server.pipe_in, server.debug_mode)
+        request = parse(JSONRPC.Request, message)
+
+        process(request, server)
+    end
+end
+
+## server options ##
+
 const TextDocumentSyncKind = Dict("None"=>0, "Full"=>1, "Incremental"=>2)
 
 type CompletionOptions 
@@ -49,36 +81,6 @@ function JSONRPC.parse_params(::Type{Val{Symbol("initialize")}}, params)
     return Any(params)
 end
 
-
-type LanguageServerInstance
-    pipe_in
-    pipe_out
-
-    rootPath::String 
-    documents::Dict{String,Document}
-    DocStore::Dict{String,Any}
-
-    debug_mode::Bool
-
-    function LanguageServerInstance(pipe_in,pipe_out, debug_mode::Bool)
-        new(pipe_in,pipe_out,"",Dict{String,Document}(),Dict{String,Any}(), debug_mode)
-    end
-end
-
-function send(message, server)
-    message_json = JSON.json(message)
-
-    write_transport_layer(server.pipe_out,message_json, server.debug_mode)
-end
-
-function Base.run(server::LanguageServerInstance)
-    while true
-        message = read_transport_layer(server.pipe_in, server.debug_mode)
-        request = parse(JSONRPC.Request, message)
-
-        process(request, server)
-    end
-end
 
 
 function process(r::JSONRPC.Request{Val{Symbol("\$/cancelRequest")},CancelParams}, server)

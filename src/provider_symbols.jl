5e550550bdaa8845efc875ec8c651ef41187cdc1
Zac Nugent <zac.nugent@gmail.com>
2016-11-29 18:41:58 +0000

    fix document symbols

--- src/provider_symbols.jl
+++ src/provider_symbols.jl
@@ -7,10 +7,9 @@ end
 function process(r::JSONRPC.Request{Val{Symbol("textDocument/documentSymbol")},DocumentSymbolParams}, server) 
     uri = r.params.textDocument.uri 
     blocks = server.documents[uri].blocks 
-    lb = get_linebreaks(server.documents[uri].data)
 
     syms = SymbolInformation[]
-    getsyminfo(blocks, syms, lb, uri)
+    getsyminfo(blocks, syms, uri, server.documents[uri])
 
     response = JSONRPC.Response(get(r.id), syms) 
     send(response, server) 
@@ -20,17 +19,17 @@ function JSONRPC.parse_params(::Type{Val{Symbol("textDocument/documentSymbol")}}
     return DocumentSymbolParams(params) 
 end
 
-function getsyminfo(blocks, syms, lb, uri, prefix="") 
+function getsyminfo(blocks, syms, uri , doc, prefix="") 
     for b in blocks.args
         name, t, loc = getname(b)
         if t==:Module
-            getsyminfo(b.args[3], syms, lb, uri, string(name))
-        elseif t==:Function
-            push!(syms, SymbolInformation(string(prefix,".",name), 12, Location(uri, Range(findfirst(x->x>first(loc), lb)-1)))) 
+            getsyminfo(b.args[3], syms, uri, doc, string(name))
+        elseif t==:Function            
+            push!(syms, SymbolInformation(string(isempty(prefix) ? "" : prefix*".",name), 12, Location(uri, Range(get_position_at(doc, first(loc))[1])))) 
         elseif t==:DataType 
-            push!(syms, SymbolInformation(string(prefix,".",name), 5, Location(uri, Range(findfirst(x->x>first(loc), lb)-1)))) 
+            push!(syms, SymbolInformation(string(isempty(prefix) ? "" : prefix*".",name), 5, Location(uri, Range(get_position_at(doc, first(loc))[1])))) 
         elseif name!=:nothing 
-            push!(syms, SymbolInformation(string(prefix,".",name), 13, Location(uri, Range(findfirst(x->x>first(loc), lb)-1)))) 
+            push!(syms, SymbolInformation(string(isempty(prefix) ? "" : prefix*".",name), 13, Location(uri, Range(get_position_at(doc, first(loc))[1]))))
         end 
     end 
 end

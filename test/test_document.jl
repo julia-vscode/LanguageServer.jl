6e56c111362a7db79daf68d703e23104bb27659e
David Anthoff <anthoff@berkeley.edu>
2016-12-14 10:46:27 -0800

    Fix test errors

--- test/test_document.jl
+++ test/test_document.jl
@@ -11,7 +11,7 @@ s1 = """
 abcde
 ABCDEFG
 """
-d1 = Document(s1)
+d1 = Document("untitled", s1, false)
 @test get_text(d1) == s1
 @test get_line(d1,1) == "123456\n"
 @test get_line(d1,2) == "abcde\n"
@@ -27,7 +27,7 @@ s2 = """
 12μ456
 abηde
 ABCDEFG"""
-d2 = Document(s2)
+d2 = Document("untitled", s2, false)
 @test get_line(d2,1) == "12μ456\n"
 @test get_line(d2,2) == "abηde\n"
 @test get_line(d2,3) == "ABCDEFG"
@@ -64,24 +64,24 @@ update(d2,2,15,0,"POST")
 
 
 s3 = ""
-d3 = Document(s3)
+d3 = Document("untitled", s3, false)
 @test get_line(d3,1) == ""
 update(d3,1,1,0,"a")
 @test get_line(d3,1) == "a"
 
 s4 = "1234\r\nabcd"
-d4 = Document(s4)
+d4 = Document("untitled", s4, false)
 @test get_line(d4,1) == "1234\r\n"
 @test get_line(d4,2) == "abcd"
 @test get_line_offsets(d4) == [1,7]
 
 s5 = "1234\nabcd\n"
-d5 = Document(s5)
+d5 = Document("untitled", s5, false)
 @test get_line(d5,1) == "1234\n"
 @test get_line(d5,2) == "abcd\n"
 @test get_line_offsets(d5) == [1,6,11]
 
 s6 = "\n"
-d6 = Document(s6)
+d6 = Document("untitled", s6, false)
 @test get_line_offsets(d6) == [1,2]
 @test get_line(d6,1) == "\n"

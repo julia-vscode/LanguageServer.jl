1179d996b865452374a62f8ba6a6a0b6df031cd5
David Anthoff <anthoff@berkeley.edu>
2016-12-14 14:48:16 -0800

    Reuse uri variable

--- src/provider_misc.jl
+++ src/provider_misc.jl
@@ -40,7 +40,7 @@ function process(r::JSONRPC.Request{Val{Symbol("textDocument/didOpen")},DidOpenT
     doc = server.documents[uri]
     set_open_in_editor(doc, true)
 
-    parseblocks(r.params.textDocument.uri, server)
+    parseblocks(uri, server)
     
     if should_file_be_linted(r.params.textDocument.uri, server) 
         process_diagnostics(r.params.textDocument.uri, server) 

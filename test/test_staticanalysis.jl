c104e73f0b6b0a980d645998fa6f1bc01a787161
Zac Nugent <zac.nugent@gmail.com>
2016-12-20 23:52:00 +0000

    update tests

--- test/test_staticanalysis.jl
+++ test/test_staticanalysis.jl
@@ -18,10 +18,10 @@ blocks = Expr(:block)
 parseblocks(testtext, blocks, 0)
 blocks.typ = 0:length(testtext.data)
 
-ns = get_names(blocks, 119)
+ns = get_names(blocks, 119, server)
 
 
-@test length(ns)==6
+@test length(ns)==8
 @test ns[:a][1]==:argument
 @test parsestruct(ns[:testtype][3])==[:a=>:Any,:b=>:Int,:c=>:(Vector{Int})]
 @test parsesignature(ns[:testfunction][3].args[1])==[(:a, :Any), (:b, :Int), (:c, :testtype)]
@@ -29,9 +29,9 @@ ns = get_names(blocks, 119)
 
 @test get_type(:testtype, ns)==:DataType
 @test get_type(:c, ns)==:testtype
-@test get_type([:c,:c], ns, blocks)==Symbol("Vector{Int}")
-@test get_fields(:testtype, ns, blocks)[:c]==:(Vector{Int})
-@test sort(collect(keys(get_fields(:Expr, ns, blocks))))==sort(fieldnames(Expr))
+@test get_type([:c,:c], ns)==Symbol("Vector{Int}")
+@test get_fields(:testtype, ns)[:c]==:(Vector{Int})
+@test sort(collect(keys(get_fields(:Expr, ns))))==sort(fieldnames(Expr))
 
 shiftloc!(blocks, 5)
 @test blocks.args[1].typ==5:144

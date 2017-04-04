fdf96d6e6fd3d20d975dc776731f5eab75943dc0
David Anthoff <anthoff@berkeley.edu>
2017-04-04 12:45:19 -0700

    Fix some bugs

--- src/languageserverinstance.jl
+++ src/languageserverinstance.jl
@@ -17,7 +17,9 @@ type LanguageServerInstance
     function LanguageServerInstance(pipe_in,pipe_out, debug_mode::Bool, user_pkg_dir::AbstractString=haskey(ENV, "JULIA_PKGDIR") ? ENV["JULIA_PKGDIR"] : joinpath(homedir(),".julia"))
         cache = Dict()
 
-        new(pipe_in,pipe_out,"", Dict{String,Document}(), cache, Channel{Symbol}(128), debug_mode, false, user_pkg_dir, is_windows() ? "\\\\.\\pipe\\vscode-language-julia-lint-server-$(getpid())" : joinpath(tempname(), "vscode-language-julia-lint-server-$(getpid())"))
+        lint_pipe_name = is_windows() ? "\\\\.\\pipe\\vscode-language-julia-lint-server-$(getpid())" : joinpath(tempname(), "vscode-language-julia-lint-server-$(getpid())")
+
+        new(pipe_in,pipe_out,"", Dict{String,Document}(), cache, Channel{Symbol}(128), debug_mode, false, user_pkg_dir, lint_pipe_name)
     end
 end
 
@@ -43,7 +45,7 @@ function Base.run(server::LanguageServerInstance)
     env_new = copy(ENV)
     env_new["JULIA_PKGDIR"] = server.user_pkg_dir
 
-    lint_stdout,lint_stdin,lint_process = readandwrite(Cmd(`$JULIA_HOME/julia -e "Base.Sys.set_process_title(\"julia linter\"); using Lint; lintserver(\"$(server.lint_pipe_name)\");"`, env=env_new))
+    lint_stdout,lint_stdin,lint_process = readandwrite(Cmd(`$JULIA_HOME/julia -e "Base.Sys.set_process_title(\"julia linter\"); using Lint; lintserver(\"$(replace(server.lint_pipe_name, "\\", "\\\\"))\");"`, env=env_new))
 
     while true
         message = read_transport_layer(server.pipe_in, server.debug_mode)

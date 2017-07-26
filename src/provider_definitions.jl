cd1432c2e461d18aff8e2b600f24134dd41d1dc6
ZacNugent <zac.nugent@gmail.com>
2017-07-26 22:36:42 +0100

    Don't return default convert method for type constructors (#133)

--- src/provider_definitions.jl
+++ src/provider_definitions.jl
@@ -1,3 +1,8 @@
+# Find location of default datatype constructor
+const DefaultTypeConstructorLoc= let def = first(methods(Int))
+    Base.find_source_file(string(def.file)), def.line
+end
+
 function process(r::JSONRPC.Request{Val{Symbol("textDocument/definition")},TextDocumentPositionParams}, server)
     if !haskey(server.documents, r.params.textDocument.uri)
         send(JSONRPC.Response(get(r.id), CancelParams(get(r.id))), server)
@@ -19,6 +24,9 @@ function process(r::JSONRPC.Request{Val{Symbol("textDocument/definition")},TextD
     end
     for m in methods(x)
         file = isabspath(string(m.file)) ? string(m.file) : Base.find_source_file(string(m.file))
+        if (file, m.line) == DefaultTypeConstructorLoc
+            continue
+        end
         push!(locations, Location(is_windows() ? "file:///$(URIParser.escape(replace(file, '\\', '/')))" : "file:$(file)", Range(m.line - 1, 0, m.line, 0)))
     end
     

75665ec2c99b83b072f3cd19ef1e688262221dca
ZacNugent <zac.nugent@gmail.com>
2019-07-09 22:53:56 +0100

    fix eval block logic

--- src/requests/misc.jl
+++ src/requests/misc.jl
@@ -98,13 +98,14 @@ function process(r::JSONRPC.Request{Val{Symbol("julia/getCurrentBlockOffsetRange
                 if a.typ === CSTParser.ModuleH
                     if loc + a.args[1].fullspan + a.args[2].fullspan < offset < loc + a.args[1].fullspan + a.args[2].fullspan + a.args[3].fullspan
                         loc0 = loc +  a.args[1].fullspan + a.args[2].fullspan
+                        loc += a.args[1].fullspan + a.args[2].fullspan
                         for b in a.args[3].args
                             if loc <= offset < loc + b.fullspan
                                 p1, p2, p3 = loc + 1, loc + b.span, loc + b.fullspan
+                                break
                             end
                             loc += b.fullspan
                         end
-                        p1, p2, p3 = loc0 + 1, loc0 + a.span, loc0 + a.fullspan
                     else
                         p1, p2, p3 = loc + 1, loc + a.span, loc + a.fullspan
                     end

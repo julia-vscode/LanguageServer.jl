0c6516611dfeb7828bb41f46a7a488e9c93650ad
David Anthoff <anthoff@berkeley.edu>
2020-01-25 16:09:48 -0800

    Fix tests

--- test/test_communication.jl
+++ test/test_communication.jl
@@ -1,61 +1,54 @@
-init_request = """
+init_request = JSON.parse("""
 {
-    "jsonrpc":"2.0",
-    "id":0,
-    "method":"initialize",
-    "params":{"processId":9902,
-              "rootPath":null,
-              "rootUri":null,
-              "capabilities":{"workspace":{"applyEdit":true,"workspaceEdit":{"documentChanges":true},"didChangeConfiguration":{"dynamicRegistration":false},"didChangeWatchedFiles":{"dynamicRegistration":false},"symbol":{"dynamicRegistration":true},"executeCommand":{"dynamicRegistration":true}},"textDocument":{"synchronization":{"dynamicRegistration":true,"willSave":true,"willSaveWaitUntil":true,"didSave":true},"completion":{"dynamicRegistration":true,"completionItem":{"snippetSupport":true}},"hover":{"dynamicRegistration":true},"signatureHelp":{"dynamicRegistration":true},"references":{"dynamicRegistration":true},"documentHighlight":{"dynamicRegistration":true},"documentSymbol":{"dynamicRegistration":true},"formatting":{"dynamicRegistration":true},"rangeFormatting":{"dynamicRegistration":true},"onTypeFormatting":{"dynamicRegistration":true},"definition":{"dynamicRegistration":true},"codeAction":{"dynamicRegistration":true},"codeLens":{"dynamicRegistration":true},"documentLink":{"dynamicRegistration":true},"rename":{"dynamicRegistration":true}}},
-              "trace":"off"}
+    "processId":9902,
+    "rootPath":null,
+    "rootUri":null,
+    "capabilities":{"workspace":{"applyEdit":true,"workspaceEdit":{"documentChanges":true},"didChangeConfiguration":{"dynamicRegistration":false},"didChangeWatchedFiles":{"dynamicRegistration":false},"symbol":{"dynamicRegistration":true},"executeCommand":{"dynamicRegistration":true}},"textDocument":{"synchronization":{"dynamicRegistration":true,"willSave":true,"willSaveWaitUntil":true,"didSave":true},"completion":{"dynamicRegistration":true,"completionItem":{"snippetSupport":true}},"hover":{"dynamicRegistration":true},"signatureHelp":{"dynamicRegistration":true},"references":{"dynamicRegistration":true},"documentHighlight":{"dynamicRegistration":true},"documentSymbol":{"dynamicRegistration":true},"formatting":{"dynamicRegistration":true},"rangeFormatting":{"dynamicRegistration":true},"onTypeFormatting":{"dynamicRegistration":true},"definition":{"dynamicRegistration":true},"codeAction":{"dynamicRegistration":true},"codeLens":{"dynamicRegistration":true},"documentLink":{"dynamicRegistration":true},"rename":{"dynamicRegistration":true}}},
+    "trace":"off"
 }
-"""
+""")
 
-init_response_json = JSON.parse("""
+init_response = JSON.parse("""
 {
-    "id": 0,
-    "jsonrpc": "2.0",
-    "result": {
-        "capabilities": {
-            "textDocumentSync": 2,
-            "hoverProvider": true,
-            "completionProvider": {
-                "resolveProvider": false,
-                "triggerCharacters": [
-                    "."
-                ]
-            },
-            "signatureHelpProvider": {
-                "triggerCharacters": [
-                    "("
-                ]
-            },
-            "definitionProvider": true,
-            "typeDefinitionProvider": false,
-            "implementationProvider": false,
-            "referencesProvider": true,
-            "documentHighlightProvider": false,
-            "documentSymbolProvider": true,
-            "workspaceSymbolProvider": true,
-            "codeActionProvider": true,
-            "documentFormattingProvider": true,
-            "documentRangeFormattingProvider": false,
-            "renameProvider": true,
-            "documentLinkProvider": {
-                "resolveProvider": false
-            },
-            "colorProvider": false,
-            "executeCommandProvider": {
-                "commands": []
-            },
-            "workspace": {
-                "workspaceFolders": {
-                    "supported": true,
-                    "changeNotifications": true
-                }
-            },
-            "experimental": null
-        }
+    "capabilities": {
+        "textDocumentSync": 2,
+        "hoverProvider": true,
+        "completionProvider": {
+            "resolveProvider": false,
+            "triggerCharacters": [
+                "."
+            ]
+        },
+        "signatureHelpProvider": {
+            "triggerCharacters": [
+                "("
+            ]
+        },
+        "definitionProvider": true,
+        "typeDefinitionProvider": false,
+        "implementationProvider": false,
+        "referencesProvider": true,
+        "documentHighlightProvider": false,
+        "documentSymbolProvider": true,
+        "workspaceSymbolProvider": true,
+        "codeActionProvider": true,
+        "documentFormattingProvider": true,
+        "documentRangeFormattingProvider": false,
+        "renameProvider": true,
+        "documentLinkProvider": {
+            "resolveProvider": false
+        },
+        "colorProvider": false,
+        "executeCommandProvider": {
+            "commands": []
+        },
+        "workspace": {
+            "workspaceFolders": {
+                "supported": true,
+                "changeNotifications": true
+            }
+        },
+        "experimental": null
     }
 }
 """)
@@ -90,15 +83,14 @@ sleep(1)
 
 client = connect(global_socket_name)
 try
-    LanguageServer.write_transport_layer(client, init_request)
+    endpoint = LanguageServer.JSONRPCEndpoints.JSONRPCEndpoint(client, client)
+    run(endpoint)
 
-    msg = LanguageServer.read_transport_layer(client)
+    response = LanguageServer.JSONRPCEndpoints.send_request(endpoint, "initialize", init_request)
 
-    msg_json = JSON.parse(msg)
-
-    @test_broken init_response_json == msg_json
-    @test msg_json["result"]["capabilities"]["typeDefinitionProvider"] == false
-    @test msg_json["result"]["capabilities"]["renameProvider"] == true
+    @test_broken init_response == response
+    @test response["capabilities"]["typeDefinitionProvider"] == false
+    @test response["capabilities"]["renameProvider"] == true
 finally
     close(client)
 end

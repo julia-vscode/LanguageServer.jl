0af077f22c593d7d3da7d44ab6734343ff58c069
ZacNugent <zac.nugent@gmail.com>
2020-02-08 11:38:40 +0000

    fix file typ

--- src/requests/textdocument.jl
+++ src/requests/textdocument.jl
@@ -347,7 +347,7 @@ function parse_jmd(ps, str)
             push!(blocks, (ps.t.startbyte, CSTParser.INSTANCE(ps)))
         end
     end
-    top = EXPR(CSTParser.Block, EXPR[])
+    top = EXPR(CSTParser.FileH, EXPR[])
     if isempty(blocks)
         return top, ps
     end

6438afd116e43b53c11d881e6a85876f96da2916
ZacNugent <zac.nugent@gmail.com>
2020-02-20 22:44:33 +0000

    Fix typo

--- src/requests/textdocument.jl
+++ src/requests/textdocument.jl
@@ -55,7 +55,7 @@ function process(r::JSONRPC.Request{Val{Symbol("textDocument/didClose")},DidClos
     if is_workspace_file(doc)
         set_open_in_editor(doc, false)
     else
-        if any(d.root == doc.root && doc._open_in_editor for (uri, d::Document) in getdocuments_pair(server) if d != doc)
+        if any(d.root == doc.root && d._open_in_editor for (uri, d::Document) in getdocuments_pair(server) if d != doc)
             # If any other open document shares doc's root we just mark it as closed...
             set_open_in_editor(doc, false)
         else

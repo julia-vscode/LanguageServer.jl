5fb58c66fd3c641caf9f7999b91c858ae5f2dafe
Sebastian Pfitzner <pfitzseb@gmail.com>
2021-03-03 17:27:23 +0100

    better getModuleAt by ensuring that the offset is inside (not on the edge of) the EXPR's span

--- src/requests/features.jl
+++ src/requests/features.jl
@@ -272,8 +272,8 @@ function julia_getModuleAt_request(params::VersionedTextDocumentPositionParams, 
     if hasdocument(server, uri)
         doc = getdocument(server, uri)
         if doc._version == params.version
-            offset = get_offset2(doc, params.position.line, params.position.character, true)
-            x = get_expr(getcst(doc), offset)
+            offset = get_offset2(doc, params.position.line, params.position.character)
+            x = get_expr_or_parent(getcst(doc), offset - 1)
             if x isa EXPR
                 scope = StaticLint.retrieve_scope(x)
                 if scope !== nothing

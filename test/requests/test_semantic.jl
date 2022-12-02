a75ab25caf0a763581a08caf0dbc994b0bfbb6a9
Felipe Lema <felipe@MacBook-Pro-de-Felipe.local>
2022-12-02 10:46:10 -0300

    cleaner test output

--- test/requests/test_semantic.jl
+++ test/requests/test_semantic.jl
@@ -6,6 +6,6 @@
 a=1
     """)
     let _LS = LanguageServer
-        @test token_full_test() == _LS.SemanticTokens(UInt32[0, 0, 9, 5, _LS.semantic_token_encoding(_LS.SemanticTokenKinds.Variable)])
+        @test Int64.(token_full_test().data) == Int64.(_LS.SemanticTokens(UInt32[0, 0, 9, 5, _LS.semantic_token_encoding(_LS.SemanticTokenKinds.Variable)]).data)
     end
 end

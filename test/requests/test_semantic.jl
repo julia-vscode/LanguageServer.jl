110e98e3cf6c9156a7dccaf58683a36e14dbe460
Felipe Lema <felipe@MacBook-Pro-de-Felipe.local>
2022-12-14 10:25:02 -0300

    fix basic test

--- test/requests/test_semantic.jl
+++ test/requests/test_semantic.jl
@@ -3,9 +3,20 @@
     include("../test_shared_server.jl")
 
     doc = settestdoc("""
-a=1
+a=123
     """)
-    let _LS = LanguageServer
-        @test Int64.(token_full_test().data) == Int64.(_LS.SemanticTokens(UInt32[0, 0, 9, 5, _LS.semantic_token_encoding(_LS.SemanticTokenKinds.Variable)]).data)
+    let _LS = LanguageServer,
+        _encoding=_LS.semantic_token_encoding
+        @test Int64.(token_full_test().data) == Int64.(_LS.SemanticTokens(
+                                                                          UInt32[0, 0, # first line, first column
+                                                                                 1, # «a»
+                                                                                 _encoding(_LS.SemanticTokenKinds.Variable), 0,
+                                                                                 0, 1,
+                                                                                 1, # «=»
+                                                                                 _encoding(_LS.SemanticTokenKinds.Operator), 0,
+                                                                                 0, 2,
+                                                                                 3, # «123»
+                                                                                 _encoding(_LS.SemanticTokenKinds.Number), 0,
+                                                                                ]).data)
     end
 end
